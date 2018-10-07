5e7aa0d233c69615c45cbe74e70d438a15a6f422
fernandoleira <LeiraFernandoCortel@gmail.com>
2018-10-07 03:46:23 -0400

    App Run

--- app.py
+++ app.py
@@ -37,4 +37,6 @@ def show(location_key):
     return render_template('index.html', data=data)
 
 if __name__=="__main__":
-    app.run(debug=False)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
